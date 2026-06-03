0155b4da85c41841e570e1c06c24d3c091862ada
mfs9697 <51402338+mfs9697@users.noreply.github.com>
2026-06-03 15:47:13 +0300

    Update site URL and set base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://inmech.kyiv.ua',
+  site: 'https://mfs9697.github.io',
+  base: '/inmech-site',
   output: 'static'
 });

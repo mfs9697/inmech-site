c43109bcbe742b71dc8f57e8677c7a1ff25437d2
mfs9697 <51402338+mfs9697@users.noreply.github.com>
2026-06-05 18:24:14 +0300

    Add monographs data

---
+++ src/data/monographs.ts
@@ -0,0 +1,235 @@
+export const monographs = [
+  {
+    authors: 'Guz A.N., Bogdanov V.L., Nazarenko V.M.',
+    title: 'Fracture of Materials Under Compression Along Cracks',
+    year: 2020,
+    publisher: 'Cham: Springer International Publishing',
+    pages: '504 p.',
+    isbn: '978-3-030-51813-4',
+    url: 'https://doi.org/10.1007/978-3-030-51814-1',
+    category: 'Механіка руйнування'
+  },
+  {
+    authors: 'Rushchitsky J.J.',
+    title: 'Foundations of Mechanics of Materials',
+    year: 2021,
+    publisher: 'Copenhagen: Ventus Publishing ApS',
+    pages: '276 p.',
+    isbn: '978-87-403-3706-8',
+    url: 'https://bookboon.com/premium/books/foundations-of-mechanics-of-materials-part-1',
+    category: 'Механіка матеріалів'
+  },
+  {
+    authors: 'Grigorenko A.Ya., Müller W.H., Loza I.A.',
+    title: 'Selected Problems in the Elastodynamics of Piezoceramic Bodies',
+    year: 2021,
+    publisher: 'Advanced Structured Materials',
+    pages: '227 p.',
+    isbn: '9783030741983',
+    url: 'https://doi.org/10.1007/978-3-030-74199-0',
+    category: 'Електропружність'
+  },
+  {
+    authors: 'Cruz-Hernandez C., Martynyuk A.A., Mazko A.G.',
+    title: 'Advances in Stability and Control Theory for Uncertain Dynamical Systems',
+    year: 2021,
+    publisher: 'Stability, Oscillations and Optimization of Systems, Vol. 11',
+    pages: '340 p.',
+    isbn: '978-1-908106-73-5',
+    url: 'https://cambridgescientificpublishers.com/product/volume-eleven-advances-in-stability-and-control-theory-for-uncertain-dynamical-systems',
+    category: 'Стійкість і керування'
+  },
+  {
+    authors: 'Guz A.N.',
+    title: 'Eight Non-Classical Problems of Fracture Mechanics',
+    year: 2022,
+    publisher: 'Advanced Structured Materials',
+    pages: '400 p.',
+    isbn: '978-3030775032',
+    url: '',
+    category: 'Механіка руйнування'
+  },
+  {
+    authors: 'Guz A.N., Altenbach H.',
+    title: 'Current Research Results of the NAS of Ukraine',
+    year: 2023,
+    publisher: 'Springer',
+    pages: '585 p.',
+    isbn: '978-3031373121',
+    url: 'https://link.springer.com/book/10.1007/978-3-031-37313-8',
+    category: 'Сучасні дослідження'
+  },
+  {
+    authors: 'Altenbach H., Bogdanov V.',
+    title: 'Selected Problems of Solid Mechanics and Solving Methods',
+    year: 2024,
+    publisher: 'Advanced Structured Materials',
+    pages: '544 p.',
+    isbn: '978-3031540622',
+    url: 'https://doi.org/10.1007/978-3-031-54063-9',
+    category: 'Механіка деформівного твердого тіла'
+  },
+  {
+    authors: 'Гузь А.Н., Глухов А.Ю.',
+    title: 'Осесиммеричные волны в высокоэластичном композитном материале с начальными напряжениями',
+    year: 2020,
+    publisher: 'Київ: ТОВ «Видавничий будинок «Аванпост-Прим»',
+    pages: '94 с.',
+    isbn: '978-617-502-416-0',
+    url: '',
+    category: 'Хвилі в композитах'
+  },
+  {
+    authors: 'За ред. Загороднього А.Г., Єрмольєва Ю.М., Богданова В.Л., Єрмольєвої Т.Ю.',
+    title: 'Взаємозв’язки в системі продовольство, енергія та вода для сталого розвитку: інтегроване моделювання та надійне управління',
+    year: 2020,
+    publisher: 'Київ: Академперіодика',
+    pages: '446 с.',
+    isbn: '978-966-02-9344-1',
+    url: '',
+    category: 'Моделювання та керування'
+  },
+  {
+    authors: 'Zagorodny A.G., Ermoliev Yu.M., Bogdanov V.L., Ermolieva T.Yu.',
+    title: 'FEW Nexus for Sustainable Development: Integrated Modeling & Robust Management',
+    year: 2020,
+    publisher: 'Kyiv: Akademperiodyka',
+    pages: '446 p.',
+    isbn: '978-966-02-9344-1',
+    url: '',
+    category: 'Моделювання та керування'
+  },
+  {
+    authors: 'Грачов О.О., Хоревін В.І.',
+    title: 'Академічна наука країн світу',
+    year: 2020,
+    publisher: 'Київ: Фенікс',
+    pages: '576 с.',
+    isbn: '978-966-136-724-0',
+    url: '',
+    category: 'Історія та організація науки'
+  },
+  {
+    authors: 'Камінський А.О., Дудик М.В., Решітник Ю.В.',
+    title: 'Моделі структури привершинної області міжфазної тріщини на ламаній межі розділу матеріалів',
+    year: 2020,
+    publisher: 'Київ, Умань, Бровари: АНФ ГРУП',
+    pages: '137 с.',
+    isbn: '978-617-7252-22-0',
+    url: 'https://dspace.udpu.edu.ua/handle/123456789/13305',
+    category: 'Механіка руйнування'
+  },
+  {
+    authors: 'Martynyuk A.A., Radziszewski B., Szadkowski A.',
+    title: 'Elements of the Theory and Applications with Examples',
+    year: 2020,
+    publisher: 'Sciendo',
+    pages: '328 p.',
+    isbn: '',
+    url: 'https://doi.org/10.2478/9788366675285',
+    category: 'Стійкість і динамічні системи'
+  },
+  {
+    authors: 'Zagorodniy A.G.',
+    title: 'National Academy of Sciences of Ukraine in 1991–2021. To the 30th Anniversary of Independence of Ukraine',
+    year: 2021,
+    publisher: 'Kyiv: Akademperiodyka',
+    pages: '228 p.',
+    isbn: '978-966-360-441-1',
+    url: 'https://doi.org/10.15407/academperiodyka.441.228',
+    category: 'Історія науки'
+  },
+  {
+    authors: 'Чорноіван Ю.О.',
+    title: 'Курс вищої математики (короткий конспект лекцій)',
+    year: 2022,
+    publisher: 'Сайт дистанційної освіти КНУБіА',
+    pages: '94 с.',
+    isbn: '',
+    url: 'http://org2.knuba.edu.ua/pluginfile.php/14036/mod_resource/content/47/lect2.pdf',
+    category: 'Навчальні матеріали'
+  },
+  {
+    authors: 'Луговий П.З., Мейш В.Ф., Мейш Ю.В.',
+    title: 'Динаміка конструктивнонеоднорідних оболонкових структур',
+    year: 2022,
+    publisher: 'Київ: Видавництво Ліра',
+    pages: '326 с.',
+    isbn: '978-617-520-320-0',
+    url: '',
+    category: 'Динаміка оболонок'
+  },
+  {
+    authors: 'Aliev F.A., Larin V.B., Velieva N.I.',
+    title: 'Algorithms of the Synthesis of Optimal Regulators',
+    year: 2022,
+    publisher: 'Outskirts Press',
+    pages: '410 p.',
+    isbn: '978-197724-985-2',
+    url: '',
+    category: 'Оптимальне керування'
+  },
+  {
+    authors: 'Жук О.П., Гузь О.М., Жук Я.О.',
+    title: 'Радіаційні сили акустичного поля в рідині з включеннями',
+    year: 2023,
+    publisher: 'Київ: Альянт',
+    pages: '240 с.',
+    isbn: '978-617-7819-38-6',
+    url: '',
+    category: 'Акустика та хвильові процеси'
+  },
+  {
+    authors: 'Rushchitsky J.J.',
+    title: 'Evolution of Solitary Elastic Waves with Different Initial Profiles',
+    year: 2023,
+    publisher: 'Chapter 2 in Horizons in Physics, Vol. 312, Nova Science Publishers',
+    pages: '252 p.',
+    isbn: '979-8-89113-513-0',
+    url: '',
+    category: 'Нелінійні хвилі'
+  },
+  {
+    authors: 'Guz A.N.',
+    title: 'Elastic Waves in Bodies with Initial (Residual) Stresses',
+    year: 2024,
+    publisher: 'Київ: Академперіодика',
+    pages: '668 с.',
+    isbn: '978-966-360-519-7',
+    url: 'https://doi.org/10.15407/akademperiodyka.519.668',
+    category: 'Хвилі в тілах з початковими напруженнями'
+  },
+  {
+    authors: 'За заг. ред. Р.М. Мартиняка',
+    title: 'Контактна механіка та поверхневі явища',
+    year: 2024,
+    publisher: 'Львів: Растр-7',
+    pages: '224 с.',
+    isbn: '978-617-8537-87-6',
+    url: '',
+    category: 'Контактна механіка'
+  },
+  {
+    authors: 'Кубенко В.Д., Янчевський І.В.',
+    title: 'Дифракція акустичних хвиль на системах неоднотипних тіл',
+    year: 2024,
+    publisher: 'Київ: КПІ ім. Ігоря Сікорського, Вид-во «Політехніка»',
+    pages: '265 с.',
+    isbn: '978-966-990-114-9',
+    url: '',
+    category: 'Акустика та дифракція хвиль'
+  },
+  {
+    authors: 'Kondratenko Yu.P., Kuntsevich V.M., Chikrii A.A., Gubarev V.F.',
+    title: 'Advanced Control Systems Theory and Applications',
+    year: 2024,
+    publisher: 'River Publishers',
+    pages: '476 p.',
+    isbn: '978-877-004-308-3',
+    url: '',
+    category: 'Теорія керування'
+  }
+] as const;
+
+export const monographYears = [...new Set(monographs.map((item) => item.year))].sort((a, b) => b - a);
+export const monographCategories = [...new Set(monographs.map((item) => item.category))].sort();

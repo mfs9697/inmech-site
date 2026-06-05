725fdb055dcee7ca808a98f8d5c492354483acfb
mfs9697 <51402338+mfs9697@users.noreply.github.com>
2026-06-05 13:38:39 +0300

    Move older defense archive data into catalog data

---
+++ src/data/defenseMaterialsArchive.ts
@@ -0,0 +1,217 @@
+export const defenseMaterialsArchive = [
+  {
+    year: '2021',
+    name: 'Яремченко Сергій Миколайович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/26ref.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/26dis.pdf']
+    ]
+  },
+  {
+    year: '2021',
+    name: 'Мельник Всеволод Михайлович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/25ref.docx'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/25dis.docx']
+    ]
+  },
+  {
+    year: '2021',
+    name: 'Орленко Сергій Петрович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/24ref.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2021/24dis.pdf'],
+      ['Відео', 'https://youtu.be/fIqUI6MljMs']
+    ]
+  },
+  {
+    year: '2020',
+    name: 'Орленко Сергій Петрович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2020/23ref.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2020/23dis.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Тімохін Олексій Павлович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/22ref.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/22dis.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Нефьодов Олександр Олексійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/21ref.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/21dis_last.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Глухов Андрій Юрійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/20ref.rar'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/20dis.zip']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Погребняк Анатолій Дмитрович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/3doc/postgraduate/dis/2019/19ref.rar'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/19dis.rar']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Рожок Лілія Степанівна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/18refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/18dis.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Шугайло Олексій Петрович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/17refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/17dis.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Юрчук Василь Миколайович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/16refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/16dis.pdf']
+    ]
+  },
+  {
+    year: '2019',
+    name: 'Хорошун Анатолій Сергійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/15refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2019/15dis.pdf']
+    ]
+  },
+  {
+    year: '2018',
+    name: 'Чернобай Володимир Сергійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/14refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/14dis.pdf']
+    ]
+  },
+  {
+    year: '2018',
+    name: 'Панкратьєв Сергій Анатолійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/13refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/13dis.pdf']
+    ]
+  },
+  {
+    year: '2018',
+    name: 'Забуга Артем Геннадійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/12refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2018/12dis.pdf']
+    ]
+  },
+  {
+    year: '2017',
+    name: 'Селіванов Михайло Федорович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2017/11refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2017/11dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Крицький Володимир Борисович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/10refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/10dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Пославський Сергій Юрійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/09refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/09dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Скосаренко Юрій Валентинович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/08refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/08dis.zip']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Вельмагіна Наталя Олександрівна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/07refer1.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/07dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Кіпніс Олександр Леонідович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/06refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/06dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Мейш Юлія Анатоліївна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/05refer1.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/05dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Семенович Катерина Олексіївна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/04refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/04dis.pdf']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Рагуліна Віра Сергіївна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/03refer.doc'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/03dis.doc']
+    ]
+  },
+  {
+    year: '2016',
+    name: 'Борисенко Максим Юрійович',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/02refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2016/02dis.pdf']
+    ]
+  },
+  {
+    year: '2015',
+    name: 'Кепенач Наталiя Павлiвна',
+    links: [
+      ['Автореферат', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2015/01refer.pdf'],
+      ['Дисертація', 'https://inmech.kyiv.ua/doc/postgraduate/dis/2015/01dis.pdf']
+    ]
+  }
+] as const;
+
+export const defenseMaterialsArchiveGroups = [...defenseMaterialsArchive.reduce((acc, item) => {
+  if (!acc.has(item.year)) acc.set(item.year, []);
+  acc.get(item.year)?.push(item);
+  return acc;
+}, new Map<string, typeof defenseMaterialsArchive>())].sort(([a], [b]) => Number(b) - Number(a));

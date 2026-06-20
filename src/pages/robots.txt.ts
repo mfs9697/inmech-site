b55e1a935d039e3062f966d945d667fbdc55f7e1
mfs9697 <51402338+mfs9697@users.noreply.github.com>
2026-06-20 18:34:03 +0300

    Add robots.txt endpoint

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ site }) => {
+  const origin = site?.origin ?? 'https://new.inmech.kyiv.ua';
+  const sitemapUrl = new URL('/sitemap-index.xml', origin).href;
+
+  return new Response(`User-agent: *\nAllow: /\n\nSitemap: ${sitemapUrl}\n`, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8'
+    }
+  });
+};

7a7d41ef9ef1029c9d1e3c572101434bd5d95b3e
mfs9697 <51402338+mfs9697@users.noreply.github.com>
2026-06-20 18:33:56 +0300

    Enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 const isInmechDeploy = process.env.DEPLOY_TARGET === 'inmech';
 
 export default defineConfig({
   site: isInmechDeploy ? 'https://new.inmech.kyiv.ua' : 'https://mfs9697.github.io',
   base: isInmechDeploy ? '/' : '/inmech-site',
-  output: 'static'
+  output: 'static',
+  integrations: [sitemap()]
 });
